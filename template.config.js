fc26e28ec8cef4bf6c83776152af111eedad80a0
chidambararaja.b <chidambararaja.b@accubits.com>
2022-03-05 01:13:43 +0530

    rn_template

--- template.config.js
+++ template.config.js
@@ -4,7 +4,7 @@ module.exports = {
   placeholderName: "ProjectName",
 
   // Directory with template
-  templateDir: "./template",
+  templateDir: "./rn_template",
 
   // Path to script, which will be executed after init
   postInitScript: "./script.js",
